20a7e18ab3a119dc0bc835db85680bb8dc0b7c18
Dante Del Favero <dante.delfavero@gmail.com>
2016-12-12 21:30:01 +0100

    reverted to  6c94f1445f44a7d9f5154a88404a036ee117da9c

---
+++ src/app/common/utils.js
@@ -0,0 +1,42 @@
+﻿
+var u_timeToString = function (dateTime) {
+    var d = new Date(dateTime);
+    var minutes = d.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    return d.getHours() + ":" + minutes;
+
+};
+
+var u_reverse = function (itemA, itemB) {
+    if (itemA.time > itemB.time)
+        return -1;
+    else if (itemA.time < itemB.time)
+        return 1;
+    else
+        return 0;
+
+};
+
+//From http://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript-which-contains-comma-in-data
+// Return array of string values, or NULL if CSV string not well formed.
+var u_csvToArray = function (text) {
+    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+    // Return NULL if input string is not well formed CSV string.
+    if (!re_valid.test(text)) return null;
+    var a = [];                     // Initialize array to receive values.
+    text.replace(re_value, // "Walk" the string using replace with callback.
+        function (m0, m1, m2, m3) {
+            // Remove backslash from \' in single quoted values.
+            if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+                // Remove backslash from \" in double quoted values.
+            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+            else if (m3 !== undefined) a.push(m3);
+            return ''; // Return empty string.
+        });
+    // Handle special case of empty last value.
+    if (/,\s*$/.test(text)) a.push('');
+    return a;
+};
